f0cce23d7f22e65bd301583d478b6787eec3d481
brian piercy <bjpcjp@gmail.com>
2021-04-29 06:30:02 -0500

    rebuild: chap 01

--- ch01-intro.py
+++ ch01-intro.py
@@ -1,16 +1,19 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# # Chapter 1: Computing with Python
+# ### Chapter 1: Computing with Python
 
 # ### Overview: a typical Python-based scientific computing stack.
 # ![software stack](pics/software-stack-overview.png)
 # 
-# * Resources:
+# ### Resources:
+# - [Intel MKL (Math Kernel Library)](https://software.intel.com/en-us/intel-mkl)
+# - [openBLAS](https://www.openblas.net)
+# - [ATLAS](https://math-atlas.sourceforge.net)
 # - [SciPy](http://www.scipy.org)
 # - [Python Numeric & Scientific topics](http://wiki.python.org/moin/NumericAndScientific)
 
-# ## Interpreter
+# ### Interpreter
 # - The easist way to execute Python code: run the program directly.
 # - Use Jupyter magic command to write Python source file to disk:
 
@@ -34,69 +37,64 @@ get_ipython().system('python hello.py')
 get_ipython().system('python --version')
 
 
-# ## Input and output caching
+# ### Input and output caching
 # 
 # * Input & output history can be accessed using __In__ (a list) & __Out__ (a dictionary). Both can be indexed with a cell number. 
 
 # In[4]:
 
 
-3 * 3
-
-
-# In[5]:
-
-
+3*3
 In[1]
 
 
 # * A single underscore = the most recent output; 
 # * A double underscore = the _next_ most recent output.
 
-# In[6]:
+# In[5]:
 
 
 1+1
 
 
-# In[7]:
+# In[6]:
 
 
 2+2
 
 
-# In[8]:
+# In[7]:
 
 
 _, __
 
 
-# In[9]:
+# In[8]:
 
 
 # In = a list
 In
 
 
-# In[10]:
+# In[9]:
 
 
 # Out = a dictionary
 Out
 
 
-# In[11]:
+# In[10]:
 
 
 # Suppress output results by ending statement with a semicolon
 1+2;
 
 
-# ## Autocompletion
+# ### Autocompletion
 # 
 # * The __Tab__ key activates autocompletion (displays list of symbol names that are valid completions of what has been typed thus far.)
 
-# In[12]:
+# In[11]:
 
 
 import os
@@ -106,106 +104,82 @@ import os
 # 
 # ![autocompletion](pics/autocompletion.png)
 
-# ## Documentation
+# ### Documentation
 # 
 # * "Docstrings" provide a built-in reference manual for most Python modules. Display the docstring by appending a Python object with "?".
+
+# In[12]:
+
+
+import math
+get_ipython().run_line_magic('pinfo', 'math.cos')
+
+
+# ### Interaction with System Shell
+# 
+# * Anything after ! is evaluated using the system shell, such as bash.
+# * (I use Ubuntu Linux as my laptop OS. Your Windows equivalents will vary.)
 
 # In[13]:
 
 
-import math
+get_ipython().system('touch file1.py file2.py file3.py')
+get_ipython().system('ls file*')
 
 
 # In[14]:
 
 
-get_ipython().run_line_magic('pinfo', 'math.cos')
+# output of a system shell command can be captured in a Python variable
+files = get_ipython().getoutput('ls file*')
+print(len(files))
+print(files)
 
-
-# ## Interaction with System Shell
-# 
-# * Anything after ! is evaluated using the system shell, such as bash.
-# * (I use Ubuntu Linux as my laptop OS. Your Windows equivalents will vary.)
 
 # In[15]:
-
-
-get_ipython().system('touch file1.py file2.py file3.py')
-
-
-# In[16]:
-
-
-get_ipython().system('ls file*')
-
-
-# In[17]:
-
-
-# output of a system shell command
-# can be captured in a Python variable
-files = get_ipython().getoutput('ls file*')
-
-
-# In[18]:
-
-
-len(files)
-
-
-# In[19]:
-
-
-files
-
-
-# In[20]:
 
 
 # pass Python variable values to shell commands
 # by prefixing the variable name with $.
 file = "file1.py"
-
-
-# In[21]:
-
-
 get_ipython().system('ls -l $file')
 
 
-# ## IPython Extensions
+# ### IPython Extensions
 # 
 # * Commands start with one or two "%" characters. A single % is used for single-line commands; dual %% is used for cells (multiple lines).
 # 
-# * %lsmagic returns a list of available commands.
+# * `%lsmagic` returns a list of available commands.
 
-# In[23]:
-
-
-get_ipython().run_line_magic('pinfo', '%lsmagic')
+# In[16]:
 
 
-# ## Running scripts
+get_ipython().run_line_magic('lsmagic', '')
 
-# In[24]:
+
+# ### Running scripts
+# 
+# - `%run` executes an external Python source file within an interactive IPython session.
+
+# In[17]:
 
 
 get_ipython().run_cell_magic('writefile', 'fib.py', '\ndef fib(N): \n    """ \n    Return a list of the first N Fibonacci numbers.\n    """ \n    f0, f1 = 0, 1\n    f = [1] * N\n    for n in range(1, N):\n        f[n] = f0 + f1\n        f0, f1 = f1, f[n]\n\n    return f\n\nprint(fib(10))')
 
 
-# In[25]:
+# In[18]:
 
 
 get_ipython().system('python fib.py')
 
 
-# In[26]:
+# In[19]:
 
 
 get_ipython().run_line_magic('run', 'fib.py')
 
 
-# In[27]:
+# In[20]:
 
 
 fib(6)
@@ -213,16 +187,16 @@ fib(6)
 
 # ## Listing all defined symbols
 # 
-# * __%who__ lists all defined symbols
-# * __%whos__ provides more detailed info.
+# * `%who` lists all defined symbols
+# * `%whos` provides more detailed info.
 
-# In[28]:
+# In[21]:
 
 
 get_ipython().run_line_magic('who', '')
 
 
-# In[29]:
+# In[22]:
 
 
 get_ipython().run_line_magic('whos', '')
@@ -230,24 +204,32 @@ get_ipython().run_line_magic('whos', '')
 
 # ## Debugger
 # 
-# * Use __%debug__ to step directly into the Python debugger.
+# * Use `%debug` to step directly into the Python debugger.
 
-# In[30]:
+# In[23]:
 
 
 # fib function fails - can't use floating point numbers.
-fib(1.0)
+try:
+    fib(1.0)
+except TypeError:
+    print("nope. can't do that.")
 
 
-# In[32]:
+# In[24]:
 
 
-get_ipython().run_line_magic('debug', '')
+#%debug
 
 
-# ## Resetting the Python namespace
+# ### Resetting the Python namespace
+# - Ensures a program is run in a pristine environment, uncluttered by existing variables and functions. Although it is necessary to reimport modules, it is important to know that even if the modules have changed since the last
+# import, a new import after a %reset will not import the new module but rather reenable a cached version from the previous import. 
+# 
+# - When developing Python modules, this is usually not the desired behavior. In that case, a reimport of a previously imported (and since updated) module can often be achieved by using the reload function from `IPython.lib.deepreload`. However, this method does not always work, as
+# some libraries run code at import time that is only intended to run once. In this case, the only option might be to terminate and restart the IPython interpreter.
 
-# In[34]:
+# In[26]:
 
 
 get_ipython().run_line_magic('reset', '')
@@ -255,9 +237,9 @@ get_ipython().run_line_magic('reset', '')
 
 # ## Timing and profiling code
 # 
-# * __%timeit__ and __%time__ provide simple benchmarking utilities.
+# * `%timeit` and `%time` provide simple benchmarking utilities.
 
-# In[35]:
+# In[25]:
 
 
 # first, re-define fibonacci code used above.
@@ -274,20 +256,21 @@ def fib(N):
     return f
 
 
-# In[36]:
+# In[26]:
 
 
+# timeit does not return expression's resulting value.
 get_ipython().run_line_magic('timeit', 'fib(50)')
 
 
-# In[37]:
+# In[27]:
 
 
 # %time only runs once. less accurate estimate.
 result = get_ipython().run_line_magic('time', 'fib(100)')
 
 
-# In[38]:
+# In[28]:
 
 
 len(result)
@@ -295,7 +278,7 @@ len(result)
 
 # * The __cProfile__ module provides __%prun__ (for statements) and __%run__ (for external scripts) profiling commands.
 
-# In[39]:
+# In[29]:
 
 
 import numpy as np
@@ -310,7 +293,7 @@ def random_walker_max_distance(M, N):
     return np.max(np.abs(trajectories))
 
 
-# In[40]:
+# In[30]:
 
 
 # returns call counts, runtime & cume runtime for
@@ -320,13 +303,13 @@ get_ipython().run_line_magic('prun', 'random_walker_max_distance(400, 10000)')
 
 # ### Jupyter: External image rendering
 
-# In[41]:
+# In[31]:
 
 
 from IPython.display import display, Image, HTML, Math
 
 
-# In[42]:
+# In[32]:
 
 
 Image(url='http://python.org/images/python-logo.gif')
@@ -334,7 +317,7 @@ Image(url='http://python.org/images/python-logo.gif')
 
 # ### Jupyter: HTML rendering
 
-# In[47]:
+# In[33]:
 
 
 import scipy, numpy, matplotlib
@@ -348,13 +331,13 @@ rows = "\n".join(
 table = "<table><tr><th>Library</th><th>Version</th></tr> %s </table>" % rows
 
 
-# In[48]:
+# In[34]:
 
 
 HTML(table)
 
 
-# In[50]:
+# In[35]:
 
 
 # another method
@@ -369,7 +352,7 @@ HTMLdisplayer(table)
 
 # ### Jupyter: Formula rendering using Latex
 
-# In[53]:
+# In[36]:
 
 
 Math(r'\hat{H} = -\frac{1}{2}\epsilon \hat{\sigma}_z-\frac{1}{2}\delta \hat{\sigma}_x')
@@ -379,7 +362,7 @@ Math(r'\hat{H} = -\frac{1}{2}\epsilon \hat{\sigma}_z-\frac{1}{2}\delta \hat{\sig
 # 
 # ** Needs debugging: slider widget doesn't appear. **
 
-# In[54]:
+# In[37]:
 
 
 import matplotlib.pyplot as plt
@@ -401,32 +384,32 @@ def f(mu):
     return fig
 
 
-# In[55]:
+# In[38]:
 
 
 from ipywidgets import interact
 import ipywidgets as widgets
 
 
-# In[58]:
+# In[39]:
 
 
 interact(f, mu=widgets.FloatSlider(min=1.0, max=20.0, step=1.0));
 
 
-# ## nbconvert to HTML file
+# ### nbconvert to HTML
 
-# In[59]:
+# In[40]:
 
 
 get_ipython().system('jupyter nbconvert --to html ch01-intro.ipynb')
 
 
-# ## nbconvert to PDF file
+# ### nbconvert to PDF
 # * [Requires a LaTeX environment](https://nbconvert.readthedocs.io/en/latest/install.html#installing-tex) to be installed.
 # * On this system (Ubuntu Linux): ```sudo apt-get install texlive-xetex```
 
-# In[60]:
+# In[41]:
 
 
 get_ipython().system('jupyter nbconvert --to pdf ch01-intro.ipynb;')
@@ -434,14 +417,20 @@ get_ipython().system('jupyter nbconvert --to pdf ch01-intro.ipynb;')
 
 # ## nbconvert to pure Python source code
 
-# In[4]:
+# In[70]:
 
 
 get_ipython().system('jupyter nbconvert ch01-intro.ipynb --to python')
 
 
-# In[5]:
+# In[71]:
 
 
 get_ipython().system('ls ch01*')
+
+
+# In[ ]:
+
+
+
 
